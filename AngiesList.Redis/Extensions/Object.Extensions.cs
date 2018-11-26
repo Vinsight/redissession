0c6d0faee52d62c25b50bd2c3cc1f38cb0bd6ca7
Eddie Loeffen <eddieloeffen@gmail.com>
2018-11-27 10:14:23 +1300

    Object helper extensions

---
+++ AngiesList.Redis/Extensions/Object.Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace AngiesList.Redis.Extensions
+{
+    public static class ObjectExtensions
+    {
+
+        public static object Get(this IDictionary<string, object> obj, string key)
+        {
+            return obj.ContainsKey(key) ? obj[key] : null;
+        }
+        /*public static object Get(this object obj, string key)
+        {
+            var dictionary = obj.ToDictionary();
+            return obj.HasProperty(key) ? dictionary[key] : null;
+        }
+
+         * */
+
+        //helper
+        public static IDictionary<string, object> ToDictionary(this object obj)
+        {
+            IDictionary<string, object> result = new Dictionary<string, object>();
+            var properties = obj.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                result.Add(property.Name, property.GetValue(obj));
+            }
+            return result;
+        }
+    }
+
+}
